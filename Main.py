230cc077ee3324995fd99fa7c82dd872c3d08399
Andrii Kudenko <kudenko4614@gmail.com>
2022-10-28 21:23:51 -0400

    adding class application

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+class Application:
+    def creating(self):
+        pcode = int(input("Enter the product code: "))
+        pname = str(input("Enter the product name: "))
+        pprice = int(input("Enter the product sale price: "))
+        pmancost = int(input("Enter the product manufacture code: "))
+        pstock = int(input("Enter the product stock level: "))
+        pmonthlyman = int(input("Enter the estimated monthly units manufactured: "))
+
