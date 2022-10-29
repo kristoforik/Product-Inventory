3c3a68734e000aa3b0167fae057e32610b360086
Andrii Kudenko <kudenko4614@gmail.com>
2022-10-28 22:29:35 -0400

    improved discription of a product

--- Main.py
+++ Main.py
@@ -8,20 +8,21 @@ class Product:
         self.pstock = stock
         self.pmonthlyman = monthlyman
     def description(self):
+        print("\n--Programming Principles Sample Stock Statement--\n")
+        print('\u001b[0m Product Code:\033[92m', self.pcode)
+        print('\u001b[0m Product Name:\033[92m', self.pname)
+        print('\u001b[0m\n Sale Price:\033[92m', self.pprice, '\u001b[0mCAD')
+        print(' Manufacture Cost:\033[92m', self.pmancost, '\u001b[0mCAD')
+        print(' In Stock:\033[92m', self.pstock, '\u001b[0munits')
+        print(' Monthly Production:\033[92m', self.pmonthlyman, '\u001b[0munits')
         print("-------------------------------------------------")
-        print('The product code is:', self.pcode)
-        print('The product name is:', self.pname)
-        print('The product price is:', self.pprice)
-        print('The product manufacture cost is:', self.pmancost)
-        print('The product stock level is:', self.pstock)
-        print('The estimated monthly units manufacture is:', self.pmonthlyman)
         return "-------------------------------------------------"
 class Application:
     def creating(self):
-        pcode = int(input("Enter the product code: "))#100-1000
+        pcode = int(input("\u001b[0mEnter the product code: "))#100-1000
         pname = str(input("Enter the product name: "))#name
-        pprice = int(input("Enter the product sale price: "))#Real number>0
-        pmancost = int(input("Enter the product manufacture cost: "))#Real number>0
+        pprice = float(input("Enter the product sale price: "))#Real number>0
+        pmancost = float(input("Enter the product manufacture cost: "))#Real number>0
         pstock = int(input("Enter the product stock level: "))#int>0
         pmonthlpyman = int(input("Enter the estimated monthly units manufactured: "))#int>=0
         return Product(pcode, pname, pprice, pmancost, pstock, pmonthlpyman).description()
