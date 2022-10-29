6057c4bdfb12665658177bcd328f57689437f315
Andrii Kudenko <kudenko4614@gmail.com>
2022-10-28 21:54:23 -0400

    trying to add discription

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-
+def add():
+    print("cool")
 class Product:
     def __init__(self, code, name, price, mancost, stock, monthlyman):
         self.pcode = code
@@ -7,16 +8,24 @@ class Product:
         self.pmancost = mancost
         self.pstock = stock
         self.pmonthlyman = monthlyman
+    def description(self):
+        print('The product code is:', self.pcode)
+        print('The product name is:', self.pname)
+        print('The product price is:', self.pprice)
+        print('The product manufacture cost is:', self.pmancost)
+        print('The product stock level is:', self.pstock)
+        print('The estimated monthly units manufacture is:', self.pmonthlyman)
     def __str__(self):
-        return self.pcode + self.pname + self.pprice + self.pmancost + self.pstock + self.pmonthlyman
-
-
+        #return str(self.pcode) + self.pname + str(self.pprice) + str(self.pmancost) + str(self.pstock) + str(self.pmonthlyman)
+        return add()
 class Application:
     def creating(self):
         pcode = int(input("Enter the product code: "))#100-1000
         pname = str(input("Enter the product name: "))#name
         pprice = int(input("Enter the product sale price: "))#Real number>0
-        pmancost = int(input("Enter the product manufacture code: "))#Real number>0
+        pmancost = int(input("Enter the product manufacture cost: "))#Real number>0
         pstock = int(input("Enter the product stock level: "))#int>0
         pmonthlpyman = int(input("Enter the estimated monthly units manufactured: "))#int>=0
-        return Product(pcode, pname, pprice, pmancost, pstock, pmonthlpyman)
+        print(Product(pcode, pname, pprice, pmancost, pstock, pmonthlpyman))
+product1 = Product(100, 'Ball', 50, 20, 100, 10)
+product1.description()
