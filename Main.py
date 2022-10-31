a95ab1bbc111d0a82494122128fe571145402790
Andrii Kudenko <kudenko4614@gmail.com>
2022-10-30 21:40:29 -0400

    added beginning message

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
-import random
+import random, sys, time
 def selling_simulator(number):
         minim = number - 10
         maxim = number + 10
         production = random.randint(minim, maxim)
         return production
-
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
 def simulating(self):
         total_sold = 0
         price = self.pprice
@@ -16,18 +20,18 @@ def simulating(self):
             sold = selling_simulator(self.pmonthlyman)
             left_in_stock = (self.pstock + self.pmonthlyman) - sold
             print("Month", n, ':')
-            print("     Manufactured:", manufactured, 'units')
-            print("     Sold:", sold, 'units')
-            print("     Stock:", left_in_stock, 'units')
+            print("|     Manufactured:", manufactured, 'units')
+            print("|     Sold:", sold, 'units')
+            print("|     Stock:", left_in_stock, 'units')
             total_sold += sold
             total_manufactured += manufactured
             self.pstock = left_in_stock
             n += 1
         net = (total_sold * price) - (total_manufactured * man_cost)
         net1 = round(net, 2)
-        print('Units manufactured:',total_manufactured)
-        print('Units sold:', total_sold)
-        print('Net Profit:', net1, 'CAD')
+        print('\033[1mUnits manufactured:\033[96m',total_manufactured, 'units')
+        print('\u001b[0m\033[1mUnits sold:\033[96m', total_sold, 'units')
+        print('\u001b[0m\033[1mNet Profit:\033[92m', net1, 'CAD\u001b[0m')
         
 class Product:
     def __init__(self, code, name, price, mancost, stock, monthlyman):
@@ -39,12 +43,12 @@ class Product:
         self.pmonthlyman = monthlyman
     def description(self):
         print("\n--Programming Principles Sample Stock Statement--\n")
-        print('\u001b[0m Product Code:\033[92m', self.pcode)
-        print('\u001b[0m Product Name:\033[92m', self.pname)
-        print('\u001b[0m\n Sale Price:\033[92m', self.pprice, '\u001b[0mCAD')
-        print(' Manufacture Cost:\033[92m', self.pmancost, '\u001b[0mCAD')
-        print(' In Stock:\033[92m', self.pstock, '\u001b[0munits')
-        print(' Monthly Production:\033[92m', self.pmonthlyman, '\u001b[0munits')
+        print('\u001b[0m Product Code:\u001b[34;1m', self.pcode)
+        print('\u001b[0m Product Name:\u001b[34;1m', self.pname)
+        print('\u001b[0m\n Sale Price:\u001b[34;1m', self.pprice, '\u001b[0mCAD')
+        print(' Manufacture Cost:\u001b[34;1m', self.pmancost, '\u001b[0mCAD')
+        print(' In Stock:\u001b[34;1m', self.pstock, '\u001b[0munits')
+        print(' Monthly Production:\u001b[34;1m', self.pmonthlyman, '\u001b[0munits')
         print("-------------------------------------------------")
         simulating(self)
         
@@ -58,7 +62,7 @@ class Product:
         return "-------------------------------------------------"
 class Application:
     def creating(self):
-            pcode = int(input("\u001b[0mEnter the product code: "))#100-1000
+            pcode = int(input("\u001b[0m\nEnter the product code: "))#100-1000
             pname = (input("Enter the product name: "))#name
             pprice = float(input("Enter the product sale price: "))#Real number>0
             pmancost = float(input("Enter the product manufacture cost: "))#Real number>0
@@ -67,7 +71,7 @@ class Application:
             return Product(pcode, pname, pprice, pmancost, pstock, pmonthlpyman).description()
 
 
-class Application1:
+'''class Application1:
     def creating(self):
             def getCode():
                 pcode = (input("Enter the product code: "))#100-1000
@@ -79,30 +83,26 @@ class Application1:
                     #while ord(i) < 65 or ord(i) > 90 and ord(i) < 97 or ord(i) > 122:
                     while ord(i)<80:
                         print('cool')
-            '''pprice = float(input("Enter the product sale price: "))#Real number>0
+            pprice = float(input("Enter the product sale price: "))#Real number>0
             
             pmancost = float(input("Enter the product manufacture cost: "))#Real number>0
             
             pstock = int(input("Enter the product stock level: "))#int>0
             
-            pmonthlpyman = int(input("Enter the estimated monthly units manufactured: "))#int>=0'''
+            pmonthlpyman = int(input("Enter the estimated monthly units manufactured: "))#int>=0
             getCode()
-            getName()
+            getName()'''
             #return Product(pcode, pname, pprice, pmancost, pstock, pmonthlpyman).description()
 
 
 
 
-product1 = Application1()
+product1 = Application()
 product2 = Product(100, 'BALL', 49.99, 19.99, 100, 100)
-product1.creating()
-'''start = ("Type [1] to create a product: ")
+print_slow("--Welcome To Programming Principles Sample Product Inventory--\n")
+time.sleep(1)
+start = int(input(("Type [1] to create a product: ")))
 if start == 1:
-    product1.creating()'''
-#pcode = (input("Enter the product code: "))
-'''for i in pname:
-                if ord(i) > 64 and ord(i) < 91 or ord(i) > 96 and ord(i) < 123:
-                    continue
-                else:
-                    return False'''
+    product1.creating()
+
 
